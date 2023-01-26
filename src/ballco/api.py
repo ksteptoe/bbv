6b103d0961860f0276f5e37f96446eb3560caa74
Kevin Steptoe <kevin.steptoe@gmail.com>
2023-01-26 21:04:04 +0000

    Initial

--- src/ballco/api.py
+++ src/ballco/api.py
@@ -6,7 +6,13 @@
 
 import logging
 import sys
-from ballco import __version__
+
+import click
+import pandas as pd
+
+# Todo get version working
+# from ballco import __version__
+from ballco.globals import PITCH, X_OFFSET, Y_OFFSET
 
 _logger = logging.getLogger(__name__)
 
@@ -23,31 +29,146 @@ def setup_logging(loglevel):
     )
 
 
-def ballco_api(n: int, loglevel: int ):
-    """Wrapper allowing :func: $(package) to be called with string arguments in a CLI fashion
+# def ballco_api(input_filename, sheet_name, output_filename, loglevel):
+#     """Wrapper allowing :func: $(package)
+#     to be called with string arguments in a CLI fashion
+#
+#      Args:
+#         input_filename:
+#         sheet_name:
+#         output_filename:
+#         loglevel: int
+#
+#     """
+#     setup_logging(loglevel)
+#     _logger.info(f"Version: {__version__}")
+#     ballco(input_filename, sheet_name, output_filename, test)
+#     _logger.info("Script ends here")
+
+
+def bump_map(input_filename, sheet_name, output_filename, loglevel):
+    """bump_map
 
     Args:
-      n: int integer argument to calculate fibonacci
-      loglevel: int
+       input_filename:
+       output_filename:
+       sheet_name:
+       output_filename:
+       loglevel: int
     """
-    setup_logging(loglevel)
-    _logger.info(f"Version: {__version__}")
-    print("The {}-th Fibonacci number is {}".format(n, ballco(n)))
-    _logger.info("Script ends here")
+
+    if output_filename is None:
+        output_filename = input_filename
+    df = pd.DataFrame()
+    try:
+        df = pd.read_excel(input_filename, sheet_name=sheet_name, skiprows=4)
+    except Exception as e:  # noqa: F841
+        click.echo(
+            click.style("f{str(e)} Error on processing {input_filename}", fg="red")
+        )
+        exit(1)
+    df.y = df.y + Y_OFFSET
+    df.x = df.x + X_OFFSET
+    odf = df[["Chip Ball Name", "x", "y"]].copy()
+    odf.loc[:, "TYPE"] = "BUMP"
+    return odf
 
 
-def ballco(n):
-    """ballco provides a Fibonacci example function
+def ball_map(input_filename, sheet_name, output_filename, loglevel):
+    """ballco
 
     Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
+       input_filename:
+       output_filename:
+       sheet_name:
+       output_filename:
+       loglevel: int
     """
-    assert n > 0
-    a, b = 1, 1
-    for _i in range(n - 1):
-        a, b = b, a + b
-    return a
+    COL_NAMES = [i for i in range(1, 17)]
+    ROW_NAMES = [chr(i) for i in range(ord("A"), ord("U"))]
+    EXCLUDE_NAMES = ("I", "O", "Q", "S")
+    ROW_NAMES = [e for e in ROW_NAMES if e not in EXCLUDE_NAMES]
+    if output_filename is None:
+        output_filename = input_filename
+    df = pd.DataFrame()
+    try:
+        df = pd.read_excel(
+            input_filename,
+            sheet_name="Ball Map",
+            usecols="C:R",
+            skiprows=[0],
+            nrows=16,
+        )
+        df.columns = COL_NAMES
+        df.index = ROW_NAMES
+        df.fillna("", inplace=True)
+    except Exception as e:  # noqa: F841
+        click.echo(
+            click.style("f{str(e)} Error on processing {input_filename}", fg="red")
+        )
+        exit(1)
+    y_ord = dict(zip(df.index, [i for i in range(len(df.index) - 1, -1, -1)]))
+    flat = df.stack().to_frame()
+    flat["Row"] = pd.Series([i[0] for i in flat.index.to_list()], index=flat.index)
+    flat["Col"] = pd.Series([i[1] for i in flat.index.to_list()], index=flat.index)
 
+    def f(x):
+        return y_ord[x]
+
+    flat["RowOrd"] = flat["Row"].apply(f)
+    flat["ColOrd"] = flat["Col"] - 1
+    flat["y"] = flat["RowOrd"] * PITCH  # + Y_OFFSET
+    flat["x"] = flat["ColOrd"] * PITCH  # + X_OFFSET
+    flat.rename(columns={0: "Chip Ball Name"}, inplace=True)
+    odf = flat[["Chip Ball Name", "x", "y"]].copy()
+    odf.loc[:, ["TYPE"]] = "BALL"
+    return odf
+
+
+# def ball_list(input_filename, sheet_name,
+#            output_filename, loglevel):
+#     """bump_map
+#
+#      Args:
+#         input_filename:
+#         output_filename:
+#         sheet_name:
+#         output_filename:
+#         loglevel: int
+#     """
+#     pitch = 65000
+#     if output_filename is None:
+#         output_filename = input_filename
+#     print(input_filename, output_filename)
+#     df = pd.DataFrame()
+#     try:
+#         df = pd.read_excel(input_filename,
+#                            sheet_name=sheet_name,
+#                            skiprows=4
+#                             )
+#     except Exception as e:
+#         click.echo(
+#             click.style(
+#                 "{} Error on processing {}".format(str(e), input_filename),
+#                 fg="red"
+#             )
+#         )
+#         exit(1)
+#     # row_col = df['Ball Ref'].str.extract(r"([A-Z])([1-9][0-9]?$)").set_axis(
+#     #     ['row_char', 'x'], axis=1).astype({'row_char': str, 'x': int})
+#     # row_col['x'] -= 1
+#     # row_col['y'] = row_col['row_char'].apply(ord).astype(int)
+#     # f = lambda x: x - 66 if (x > 79) else x - 65
+#     # row_col['y'] = row_col['y'].apply(f)
+#     # co_ords = row_col.drop(['row_char'], axis=1)
+#     # co_ords = co_ords*pitch
+#     # co_ords['y'] = co_ords['y']*-1
+#     # co_ords['y'] = co_ords['y'] + co_ords['y'].min().tolist() * -1
+#
+#     odf = pd.concat([df[['Ball Ref', 'Net Name', 'Counter']], co_ords], axis=1)
+#     plt.scatter(odf.x, odf.y)
+#     for index, row in odf.iterrows():
+#         plt.annotate(row['Ball Ref'], (row.x, row.y + 300))
+#
+#     plt.show()
+#     odf.to_excel(output_filename)
--- src/ballco/cli.py
+++ src/ballco/cli.py
@@ -4,8 +4,14 @@ which will install the command $(package) inside your current environment.
 """
 
 import logging
+from pathlib import Path
+
 import click
-from .api import ballco_api
+import matplotlib.pylab as plt
+import pandas as pd
+
+from ballco.api import ball_map, bump_map
+from ballco.globals import X_OFFSET, Y_OFFSET
 
 __author__ = "Kevin Steptoe"
 __copyright__ = "Kevin Steptoe"
@@ -17,16 +23,83 @@ _logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.version_option(__version__, '--version')
-@click.argument('n', type=int)
-@click.option('-v', '--verbose', 'loglevel', type=int, flag_value=logging.INFO)
-@click.option('-vv', '--very_verbose', 'loglevel', type=int, flag_value=logging.DEBUG)
-def cli(n, loglevel):
-    """Calls :func:`main` passing the CLI arguments extracted from click
+@click.argument("input_filename", type=click.Path(exists=True, readable=True))
+@click.argument("output_filename", type=click.Path(), required=False)
+@click.version_option(__version__, "--version")
+@click.option("-v", "--verbose", "loglevel", type=int, flag_value=logging.INFO)
+@click.option("-vv", "--very_verbose", "loglevel", type=int, flag_value=logging.DEBUG)
+@click.option(
+    "-s",
+    "--sheet",
+    "sheet_name",
+    default="Sheet1",
+    help="Sheet Name",
+    show_default=True,
+)
+def cli(
+    input_filename: Path,
+    sheet_name: str = None,
+    output_filename: str = None,
+    loglevel=logging.INFO,
+):
+    """ballco
 
-    This function can be used as entry point to create console scripts with setuptools.
+    Reads an Excel file which contains ball ordering
+    Outputs ball co-ordinates
     """
-    ballco_api(n, loglevel)
+    bum = bump_map(input_filename, "Bump List", output_filename, loglevel)
+    ball = ball_map(input_filename, "Ball Map", output_filename, loglevel)
+    bum_ball = pd.concat([bum, ball], ignore_index=True)
+    bumps_on = True
+    balls_on = True
+    action = input("b[bump] B[all] e[xpression] s[status] q[uit]?:")
+    exp = ""
+    b_marker = pd.DataFrame(
+        {"Chip Ball Name": ["b_LL", "b_UR"], "x": [105, 6765], "y": [-521, 1680]}
+    )
+    b_marker.x += X_OFFSET
+    b_marker.y += Y_OFFSET
+    B_marker = pd.DataFrame(
+        {"Chip Ball Name": ["B_LL", "B_UR"], "x": [26105, 32765], "y": [64479, 66680]}
+    )
+
+    while action != "q":
+        if action == "b":
+            bumps_on = not bumps_on
+            print(f"Bumps On {bumps_on}")
+        elif action == "B":
+            balls_on = not balls_on
+            print(f"Balls On {balls_on}")
+        elif action == "s":
+            print(f"Bumps On {bumps_on} Balls On {balls_on} regex ->{exp}<-")
+        elif action == "e":
+            exp = input("Please Enter a Regular Expression or q[uit]?:")
+            while exp != "quit" or exp != "q":
+                try:
+                    disp = bum_ball[bum_ball["Chip Ball Name"].str.match(exp)]
+                    balls = disp[disp["TYPE"] == "BALL"]
+                    bumps = disp[disp["TYPE"] == "BUMP"]
+                    types = [b_marker, B_marker]
+                    if bumps_on:
+                        types.append(bumps)
+                    if balls_on:
+                        types.append(balls)
+                    for type in types:
+                        plt.scatter(type.x, type.y)
+                        for index, row in type.iterrows():
+                            plt.annotate(row[0], (row.x, row.y + 300))
+                    plt.show()
+                except Exception as e:
+                    click.echo(
+                        click.style(
+                            "{} Error on processing regex: {}".format(str(e), exp),
+                            fg="red",
+                        )
+                    )
+                exp = input("Please Enter a Regular Expression or q[uit]?:")
+                if exp == "q" or exp == "quit":
+                    break
+        action = input("b[bump] B[all] e[xpression] q[uit]?:")
 
 
 if __name__ == "__main__":
